3ecb2cfa32ffd3f238a3f43c5aefc06bb71360da
yaoxin <yaoxin@wondersgroup.com>
2020-09-17 17:53:10 +0800

    归并及快排

--- src/main/java/com/algorithm/all/Practice_Recursion.java
+++ src/main/java/com/algorithm/all/Practice_Recursion.java
@@ -2,16 +2,27 @@ package com.algorithm.all;
 
 /**
  * @ClassNamePractice_Recursion
- * @Description
+ * @Description 递归
  * @Author Yao Xin
  * @Date2020/9/16 17:19
  **/
 public class Practice_Recursion {
     //查询数组区间最大值
-    public int findMaxInArea(int[] arr,int left,int right){
+    public static int findMaxInArea(int[] arr,int left,int right){
         if(left == right){
             return arr[left];
         }
-        return 1;
+        int middle = left+((right-left)>>1);
+        int leftMax = findMaxInArea(arr,left,middle);
+        int rightMax = findMaxInArea(arr,middle+1,right);
+        return leftMax>rightMax?leftMax:rightMax;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,7,3,2,7,5,43,33,1,6,55};
+        System.out.println(findMaxInArea(arr,0,arr.length-1));
+
+        int[] arr1 = new int[10];
+        System.out.println(arr1.length);
     }
 }
